92a9e0eb3b8e967797fc0225870c8597d262a4b1
Fyodor Sotnikov <s8tn1kv@gmail.com>
2026-07-11 19:44:44 +0300

    v0.5.20-dev

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ pub const BANNER: &str = r#"
 //                            |_|                |___/
 "#;
 
-pub const VERSION: &str = "0.5.19-dev";
+pub const VERSION: &str = "0.5.20-dev";
 
 pub use config::{
     amnezia_wg::{
