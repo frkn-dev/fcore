1a99520c3c75fb2b2b935c815f3b21251f0819c7
Fyodor Sotnikov <s8tn1kv@gmail.com>
2026-07-28 20:31:16 +0300

    v0.6.10: per-connection service_protocol in /v1/services

--- src/bin/api/http/handlers/amnezia.rs
+++ src/bin/api/http/handlers/amnezia.rs
@@ -99,6 +99,8 @@ pub struct GatewayConnection {
     pub country_code: String,
     #[serde(rename = "country_name")]
     pub country_name: String,
+    #[serde(rename = "service_protocol")]
+    pub service_protocol: String,
     #[serde(rename = "connection_label")]
     pub connection_label: String,
 }
@@ -304,6 +306,7 @@ where
                     connection_uuid: *conn_id,
                     country_code: code.clone(),
                     country_name: code.clone(),
+                    service_protocol: proto_label(tag).to_string(),
                     connection_label: label,
                 });
             }
@@ -314,6 +317,7 @@ where
             connection_uuid: uuid::Uuid::nil(),
             country_code: String::new(),
             country_name: "All countries".to_string(),
+            service_protocol: String::new(),
             connection_label: "All countries".to_string(),
         }];
     }
